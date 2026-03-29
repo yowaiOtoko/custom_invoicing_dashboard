b98613b7bc3442372570090a5f7f3ac08455255c
yowaiotoko <bsg75g@gmail.com>
2026-03-29 19:39:38 -0400

    init

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Custom Invoicing Dashboard',
+    'version': '19.0.1.0.0',
+    'category': 'Accounting',
+    'summary': 'Minimal invoicing and quotations dashboard',
+    'license': 'LGPL-3',
+    'depends': ['account', 'sale', 'web'],
+    'data': [
+        'views/dashboard_templates.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
